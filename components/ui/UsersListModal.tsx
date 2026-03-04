190042935a8bb8d378bf452cb3cbb807c0a83f61
Surya S <157696153+surya-s-1@users.noreply.github.com>
2026-03-04 22:39:14 +0530

    small fix

--- components/ui/UsersListModal.tsx
+++ components/ui/UsersListModal.tsx
@@ -119,7 +119,7 @@ export default function UsersListModal({
                                     }
                                     : undefined
                             }
-                            hideFollowButton={isMembersModal}
+                            hideFollowButton={false}
                         />
                     ))
                 ) : (
